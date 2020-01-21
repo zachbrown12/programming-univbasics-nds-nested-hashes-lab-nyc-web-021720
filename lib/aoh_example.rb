4eac4d5b25d465c43f1b5ec503ad4819c9848b70
Zach Brown <zachmb22@gmail.com>
2020-01-21 01:42:56 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,12 +11,11 @@ def assembled_aoh
   # Array. Ruby constants are denoted by ALL_CAPS
   
   infinite = [
-    DON_G
-    JOELLE_VD
-    PAT_M 
-    KATE_G
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
     BRUCE_G
-    
     ]
   
 end
