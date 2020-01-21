5dfcf05c97b629674ad57c7175d3a543f568bb49
Zach Brown <zachmb22@gmail.com>
2020-01-21 01:44:38 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -23,6 +23,17 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  
+  infinite = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G
+    ]
+  
+  infinite
+  
 end
 
 def aoh_lookup(aoh, row, key)
